990b05e406a252f6e3eafd874a417ed26d87ceea
AlthalosV <devanclark5@gmail.com>
2019-02-14 11:08:10 -0600

    update test rational

--- test_rational.py
+++ test_rational.py
@@ -4,5 +4,8 @@ import math
 class TestRational(TestCase):
     def testAddIrrationalNumber(self):
         r = Rational();
-        self.assertRaises(Exception, r.add,  math.sqrt(-1), math.sqrt(-1));
-    pass
+        self.assertRaises(Exception, r.__add__( math.sqrt(-1), math.sqrt(-1)));
+    def testAddABoolean(self):
+        r = Rational();
+        self.assertRaises(Exception, r.__add__(true, 1));
+        pass
