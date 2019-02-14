22aa25b22cad55ac75808b1e3cefc337894bd5e3
stamate4 <stamate4@uwm.edu>
2019-02-14 12:31:26 -0600

    ?

--- test_Rational_Ben.py
+++ test_Rational_Ben.py
@@ -1,15 +1,36 @@
 from unittest import TestCase
 from Rational import Rational
 import math
-class TestRational(TestCase):
-    def testAddIrrationalNumber(self):
-        r = Rational()
-        self.assertRaises(Exception, r.__add__(math.sqrt(1)))
+class TestRational_Ben(TestCase):
 
-    def testAddABoolean(self):
-        r = Rational()
-        self.assertRaises(Exception, r.__add__(True))
+    def testToSTR(self):
+        # STR variables
+        self.default = Rational()
+        self.zero = Rational(0, 1)
+        self.num1 = Rational(1, 1)
+        self.num2 = Rational(1, 2)
+        self.numNeg = Rational(1, -2)
+        self.numReduced = Rational(2, 4)
+        self.numFloat = Rational(1, 200000)
 
-    def testDivideByZero(self):
-        r = Rational(0).
-self.assertRaises(Exception)
+        self.assertEquals(self.default.__str__(), "0")
+        self.assertEquals(self.zero.__str__(), "0")
+        self.assertEquals(self.num1.__str__(), "1")
+        self.assertEquals(self.num2.__str__(), "1/2")
+        self.assertEquals(self.numNeg.__str__(), "1/-2")
+        self.assertEquals(self.numReduced.__str__(), "1/2")
+        self.assertEquals(self.numFloat.__str__(), "0.000005")
+
+    def testToFloat(self):
+        self.defaultFloat = Rational()
+        self.floatZero = Rational(0, 1)
+        self.float1 = Rational(1, 200000)
+        self.float2 = Rational(1, 1)
+        self.float3 = Rational(1559, 4420)
+
+        self.assertEquals(self.defaultFloat.__float__(), 0.0)
+        self.assertEquals(self.floatZero.__float__(), 0.000005)
+        self.assertEquals(self.float1.__float__(), 0.000005)
+        self.assertEquals(self.float2.__float__(), 1)
+        self.assertEquals(self.float3.__float__(), 0.35271493212669683257918552036199)
+        self.assertEquals
